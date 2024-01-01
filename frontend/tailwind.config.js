cf6a9eb8631651e3c774950717ac87f3ded7debe
Michael Ivanov <michaelivanov153@gmail.com>
2024-01-01 12:06:53 -0500

    added colors object

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('/src/images/unblurred.jpg')",
+      },
+      colors: {
+        primary: '#050706'
+      }
+
+    },
   },
   plugins: [],
 }
