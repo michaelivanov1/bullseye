97078b6c3566c285fdf6d681d39de958993adf46
Michael Ivanov <michaelivanov153@gmail.com>
2024-01-02 20:15:15 -0500

    added dummy text to hero

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -3,8 +3,10 @@
 const HomePage = () => {
 
     return (
-        <div className="bg-hero-image bg-cover w-full h-screen">
-
+        <div className="relative bg-hero-image bg-cover w-full flex items-center" style={{ height: '85vh' }}>
+            <div className="absolute text-white text-left left-48">
+                <h1 className="text-4xl font-bold mb-4">Some text here eventually lol</h1>
+            </div>
         </div>
     );
 
