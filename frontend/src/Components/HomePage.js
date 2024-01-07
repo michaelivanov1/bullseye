3a574846d15acc481717ea0070f9a9b8fe1984fc
Michael Ivanov <michaelivanov153@gmail.com>
2024-01-07 13:02:24 -0500

    reworked hero, navbar

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -1,15 +1,36 @@
-
+import React from 'react';
 
 const HomePage = () => {
-
     return (
-        <div className="relative bg-hero-image bg-cover w-full flex items-center" style={{ height: '85vh' }}>
-            <div className="absolute text-white text-left left-48">
-                <h1 className="text-4xl font-bold mb-4">Some text here eventually lol</h1>
+        <div>
+            <div className="relative bg-hero-image bg-cover w-full flex" style={{ height: '100vh' }}>
+
+                <div className="w-full">
+                    <div className="mx-auto">
+                        <div className="p-10 bg-black bg-opacity-30">
+                            {/* <div className="text-white font-bold text-lg">Logo
+                            <img src="../images/logo.png" />
+                        </div> */}
+                            <ul className="flex space-x-16 justify-center">
+                                <li><a href="#" className="text-primary text-2xl">Home</a></li>
+                                <li><a href="#" className="text-primary text-2xl">About</a></li>
+                                <li><a href="#" className="text-primary text-2xl">Services</a></li>
+                                <li><a href="#" className="text-primary text-2xl">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
+            <div className="absolute text-primary text-left top-72 w-1/2 border px-40">
+                <h1 className="text-5xl uppercase" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)', lineHeight: '1.2' }}>patience is the <br />currency of success</h1>
+                <h1 className="my-10 text-header">Some dummy text here Some dummy text here Some dummy text here Some dummy text here Some dummy text here </h1>
+                <button className="text-2xl">Learn More</button>
+            </div>
+
         </div>
     );
-
 }
 
 export default HomePage;
