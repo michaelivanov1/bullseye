e85846e03e4fb16f98a346680a4cd61ccf3e45b1
DMahyuddin <mahyuddin.22dylan@gmail.com>
2024-01-14 22:14:45 -0500

    core styles made

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -1,55 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/homepage/logo.png";
 import learnmorebutton from "../images/homepage/learn-more-button.png";
 import TickerTapeWidget from "../Widgets/TickerTape/TickerTape";
 import "animate.css";
 
 const HomePage = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
   return (
     <div>
       <div
         className="relative bg-hero-image bg-cover w-full flex"
         style={{ height: "100vh" }}
       >
-
         <div className="w-full">
           <TickerTapeWidget />
           <div className="mx-auto animate__animated animate__fadeInUp">
-            <div className="p-8 bg-black bg-opacity-30">
-
-              <ul className="flex space-x-24 justify-center items-center">
-                <li>
-                  <a href="#" className="text-primary text-2xl">
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="text-primary text-2xl">
-                    Market Activity
-                  </a>
-                </li>
-                <div className="px-14">
-                  <img src={logo} alt="Logo" />
-                </div>
-                <li>
-                  <a href="#" className="text-primary text-2xl">
-                    Market News
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="text-primary text-2xl">
-                    About
-                  </a>
-                </li>
-              </ul>
+            <div className="p-8 bg-black bg-opacity-30 ">
+              <div className="flex justify-center">
+                <img src={logo} alt="Logo" />
+              </div>
+              <div className="lg:hidden">
+                <button
+                  onClick={toggleMobileMenu}
+                  className="navbar-burger flex items-center text-blue-600 p-3"
+                >
+                  <svg
+                    className="block h-6 w-6 fill-current"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Mobile menu</title>
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                  </svg>
+                </button>
+              </div>
+              <div className="lg:flex lg:space-x-24 lg:justify-center lg:items-center">
+                <ul
+                  className={`${
+                    mobileMenuOpen ? "block" : "hidden"
+                  } lg:flex lg:space-x-24 lg:items-center`}
+                >
+                  <li>
+                    <a href="#" className="text-primary text-2xl">
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="text-primary text-2xl">
+                      Market Activity
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="text-primary text-2xl">
+                      Market News
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="text-primary text-2xl">
+                      About
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="absolute text-left top-72 w-2/5 pl-28 animate__animated animate__fadeInDown">
+      <div className="absolute sm:text-left top-72 sm:w-2/5 sm:pl-28 animate__animated animate__fadeInDown">
         <h1
-          className="text-6xl text-white"
+          className="text-md lg:text-6xl text-white"
           style={{
             textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
             lineHeight: "1.2",
@@ -58,22 +83,22 @@ const HomePage = () => {
           Patience Is The <br />
           Currency Of Success
         </h1>
-        <h1 className="my-10 text-header" style={{
-          textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)",
-          lineHeight: "1.6",
-        }}>
-          A lightning-fast solution to view all market data at a glance. Explore charts, news, and more on Bullseye.
+        <h1
+          className="text-sm my-10 text-header"
+          style={{
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)",
+            lineHeight: "1.6",
+          }}
+        >
+          A lightning-fast solution to view all market data at a glance. Explore
+          charts, news, and more on Bullseye.
         </h1>
         {/* <button className="flex items-center text-2xl text-black border p-2 border-black bg-green-500  hover:bg-green-600">
           Learn More
-
         </button> */}
-
         <div className="transform hover:scale-105 transition-transform cursor-pointer inline-block max-w-max">
           <img src={learnmorebutton} />
         </div>
-
-
       </div>
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'hero-image': "url('/src/images/homepage//hero-image.jpg')",
+        "hero-image": "url('/src/images/homepage//hero-image.jpg')",
+      },
+      variants: {
+        fill: ["hover", "focus"],
       },
       colors: {
-        primary: '#26C85E',
-        header: 'white',
-        subheader: '#ADB1B3',
-      }
-
+        primary: "#26C85E",
+        header: "white",
+        subheader: "#ADB1B3",
+      },
     },
   },
   plugins: [],
-}
-
+};
