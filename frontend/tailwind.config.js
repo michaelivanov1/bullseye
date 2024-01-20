a2c5a850c617f0bb9fcf73bfec6d29c23533e32e
Michael Ivanov <michaelivanov153@gmail.com>
2024-01-20 14:23:24 -0500

    added about section img

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         "hero-image": "url('/src/images/homepage/hero-section/hero-image.jpg')",
-        "about-section-image": "url('/src/images/homepage/about-section/about-bg.png')"
+        "about-section-image": "url('/src/images/homepage/about-section/about-bg.jpg')",
       },
       variants: {
         fill: ["hover", "focus"],
@@ -14,7 +14,7 @@ module.exports = {
         primary: "#26C85E",
         secondary: "#1D2027",
         header: "white",
-        subheader: "#ADB1B3",
+        subheader: "#ADB1B3"
       },
     },
   },
