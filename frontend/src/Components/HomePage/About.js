8a8ef47cb01af0c6ba043609d9ed76e34626e2ed
Michael Ivanov <michaelivanov153@gmail.com>
2024-01-20 14:22:59 -0500

    started working on about section

--- frontend/src/Components/HomePage/About.js
+++ frontend/src/Components/HomePage/About.js
@@ -7,11 +7,16 @@ const About = ({ aboutSectionRef }) => {
 
         <div
             ref={aboutSectionRef}
-            className="bg-about-section-image w-full"
+            className="bg-about-section-image w-full h-full"
             style={{ height: "100vh" }}>
-            <h1 className="text-header">d</h1>
 
-            <TickerOverview />
+            <div className="w-1/3 border border-white h-1/2 pl-48 pt-36 text-left">
+                <h1 className="text-primary text-2xl mb-4">Why Bullseye?</h1>
+                <h1 className="text-header text-3xl mb-4">Lorem Ipsum<br/>Dummy Text</h1>
+                <h1 className="text-subheader">some dummy text here lol some dummy text here lol some dummy text here lol some dummy text here lol some dummy text here lol some dummy text here lol </h1>
+            </div>
+
+            {/* <TickerOverview /> */}
 
         </div>
     );
