085fe202642705932f28c2fb46a758ec30354aea
Michael Ivanov <michaelivanov153@gmail.com>
2024-01-28 02:11:25 -0500

    fundamentals widget

---
+++ frontend/src/Widgets/FundamentalData/FundamentalData.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useRef } from 'react';
+
+const FundamentalDataWidget = () => {
+    const container = useRef();
+    const script = useRef();
+
+    useEffect(() => {
+        if (!script.current) {
+            script.current = document.createElement('script');
+            script.current.type = 'text/javascript';
+            script.current.src = 'https://s3.tradingview.com/external-embedding/embed-widget-financials.js';
+            script.current.async = true;
+            script.current.innerHTML = `
+                {
+                    "isTransparent": true,
+                    "largeChartUrl": "",
+                    "displayMode": "regular",
+                    "width": 480,
+                    "height": 830,
+                    "colorTheme": "dark",
+                    "symbol": "NASDAQ:TSLA",
+                    "locale": "en"
+                }
+            `;
+
+            container.current.innerHTML = '';
+            container.current.appendChild(script.current);
+        }
+
+        return () => {
+            if (script.current && script.current.parentNode === document.body) {
+                document.body.removeChild(script.current);
+                script.current = null;
+            }
+        };
+    }, []);
+
+    return (
+        <div className="tradingview-widget-container" ref={container}>
+            <div className="tradingview-widget-container__widget"></div>
+            <div className="tradingview-widget-copyright">
+                <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
+                    <span className="blue-text">Track all markets on TradingView</span>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default FundamentalDataWidget;
