4ed0a2d83400e9afa601615bcb503e9270a9e043
Michael Ivanov <michaelivanov153@gmail.com>
2024-01-28 02:10:43 -0500

    minor styling edits

--- frontend/src/Components/HomePage/About.js
+++ frontend/src/Components/HomePage/About.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import TickerOverview from '../../Widgets/TickerOverview/TickerOverview';
+import richText1 from '../../images/homepage/about-section/rich-text1.png';
+import richText2 from '../../images/homepage/about-section/rich-text2.png';
+import FundamentalDataWidget from '../../Widgets/FundamentalData/FundamentalData';
+import CompanyProfileWidget from '../../Widgets/CompanyProfile/CompanyProfile';
 
 const About = ({ aboutSectionRef }) => {
     return (
@@ -11,15 +15,46 @@ const About = ({ aboutSectionRef }) => {
                 <div className="w-2/5 text-left">
                     <h1 className="text-primary text-2xl mb-4 pl-24 pt-24">Why Bullseye?</h1>
                     <h1 className="text-header text-8xl mb-4 pl-24">Simplify<br />Visualization</h1>
-                    <h1 className="text-subheader pl-24">Viewing stock market data has never been this straightforward.
-                        <br /><br />Bullseye features a rich UI with easy access to financials, charts, news, and more market-related data.</h1>
+                    <h1 className="text-subheader pl-24">
+                        Bullseye features a rich UI with easy access to financials, charts, news, and more market-related data.</h1>
+
+                    <div className="pl-24 pt-20 flex w-10/12">
+                        <img className="w-12 h-12" src={richText1} />
+                        <div className="flex flex-col">
+                            <h1 className="text-header pl-12">Real-Time Updates</h1>
+                            <h1 className="text-subheader pl-12">Stay ahead of market trends with real-time updates on stock prices, news, and key financial data</h1>
+                        </div>
+                    </div>
+
+                    <div className="pl-24 pt-8 flex w-10/12">
+                        <img className="w-12 h-12" src={richText2} />
+                        <div className="flex flex-col">
+                            <h1 className="text-header pl-12">Second Opinions</h1>
+                            <h1 className="text-subheader pl-12">Read what analysts think of companies but viewing Buy, Hold, & Sell recommendations</h1>
+                        </div>
+                    </div>
+
                 </div>
 
-                <div className="w-3/5 text-left flex p-24">
+                <div className="w-3/5 text-left flex p-36">
                     <TickerOverview />
                 </div>
             </div>
 
+            <div className="bg-about-section-banner flex h-screen">
+
+                <div className="w-2/5 text-left p-24">
+                    <h1 className="text-header text-6xl">Dive Deep</h1>
+                    <h1 className="text-subheader text-3xl pt-4">Into company numbers</h1>
+                </div>
+
+                <div className="w-3/5 text-left flex">
+                    <CompanyProfileWidget />
+                    <FundamentalDataWidget />
+                </div>
+
+
+            </div>
         </div>
     );
 };
--- frontend/src/Components/HomePage/HomePage.js
+++ frontend/src/Components/HomePage/HomePage.js
@@ -1,33 +1,18 @@
+// HomePage.js
 import React, { useRef } from "react";
-// import learnmorebutton from "../../images/homepage/hero-section/learn-more-button.png";
 import TickerTapeWidget from "../../Widgets/TickerTape/TickerTape";
-import TickerOverview from "../../Widgets/TickerOverview/TickerOverview";
-import SingleTickerWidget from "../../Widgets/SingleTicker/SingleTicker";
 import StockMarketWidget from "../../Widgets/StockMarket/StockMarket";
 import MarketStatus from "../../Helpers/MarketStatus";
-import Navbar from "./Navbar";
+import Navbar from "../Navbar/Navbar";
 import "animate.css";
 
-import About from "./About";
-
-const HomePage = () => {
-  const aboutSectionRef = useRef();
-
-  const handleButtonClick = () => {
-    aboutSectionRef.current.scrollIntoView({ behavior: 'smooth' });
-  };
-
-
-
-
+const HomePage = ({ onLearnMoreClick }) => {
   return (
     <div>
       <div className="relative bg-hero-image bg-cover w-full h-screen">
         <TickerTapeWidget />
         <Navbar />
         <div className="w-full flex" style={{ height: '80vh' }}>
-
-
           <div className="w-2/5 sm:pl-28 pt-24 animate__animated animate__fadeInDown text-left">
             <h1 className="text-md lg:text-7xl text-header ">
               Patience is the Currency of Success
@@ -38,7 +23,7 @@ const HomePage = () => {
               charts, news, and more on Bullseye.
             </h1>
 
-            <button onClick={handleButtonClick} className="w-[200px] bg-transparent border-2 border-green-500 text-primary rounded-full p-3 text-2xl hover:bg-green-500 hover:text-white transition-all duration-300">
+            <button onClick={onLearnMoreClick} className="w-[200px] bg-transparent border-2 border-green-500 text-primary rounded-full p-3 text-2xl hover:bg-green-500 hover:text-white transition-all duration-300">
               Learn More
             </button>
           </div>
@@ -51,48 +36,8 @@ const HomePage = () => {
           </div>
         </div>
       </div>
-
-      <About aboutSectionRef={aboutSectionRef} />
     </div>
   );
-
-
-
-
-
-
-
-  // return (
-  //   <div>
-  //     <div className="relative bg-hero-image bg-cover w-full flex h-screen">
-  //       <div className="w-full">
-  //         <TickerTapeWidget />
-  //         <div className="mx-auto animate__animated animate__fadeInUp">
-  //           <Navbar />
-  //         </div>
-  //       </div>
-  //     </div>
-
-  //     <div className="absolute sm:text-left top-72 sm:w-2/5 sm:pl-28 animate__animated animate__fadeInDown">
-  //       <h1 className="text-md lg:text-7xl text-header">
-  //         Patience Is The
-  //         Currency Of Success
-  //       </h1>
-  //       <h1
-  //         className="text-subheader my-10">
-  //         A lightning-fast solution to view all market data at a glance. Explore
-  //         charts, news, and more on Bullseye.
-  //       </h1>
-  //       <div className="transform hover:scale-105 transition-transform cursor-pointer inline-block max-w-max">
-  //       </div>
-  //       <button onClick={handleButtonClick} class="w-[200px] bg-transparent border-2 border-green-500 text-primary rounded-full p-3 text-2xl hover:bg-green-500 hover:text-white transition-all duration-300">
-  //         Learn More
-  //       </button>
-  //     </div>
-
-  //     <About aboutSectionRef={aboutSectionRef} />
-  //   </div>
-  // );
 };
 
 export default HomePage;
