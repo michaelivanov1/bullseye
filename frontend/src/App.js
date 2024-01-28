865767b84845354158740d5867a9ddba836d8f4b
Michael Ivanov <michaelivanov153@gmail.com>
2024-01-28 02:10:28 -0500

    added about component to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
+import React, { useState, useRef } from 'react';
 import './App.css';
 import HomePage from './Components/HomePage/HomePage';
+import Navbar from './Components/Navbar/Navbar';
+import About from './Components/HomePage/About';
+import Footer from './Components/Footer/Footer';
 
 function App() {
+
+  const aboutSectionRef = useRef();
+
+  const handleButtonClick = () => {
+    aboutSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
-    /* fontFamily: 'Livvic is also good*/
     <div className="App" style={{ fontFamily: 'Poppins, sans-serif' }}>
-      <div>
-        <HomePage />
-      </div>
+      <HomePage onLearnMoreClick={handleButtonClick} />
+      <About aboutSectionRef={aboutSectionRef} />
+      {/* <Footer /> */}
     </div>
   );
 }
