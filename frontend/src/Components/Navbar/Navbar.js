abf104ed0651e181e0249e28eee6f8247c4b5b65
Michael Ivanov <michaelivanov153@gmail.com>
2024-02-03 19:58:03 -0500

    modified navbar size to reduce padding

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
     };
 
     return (
-        <div className="p-8 bg-black bg-opacity-20"  style={{ height: '15vh' }}>
+        <div className="p-6 bg-black bg-opacity-20"  style={{ height: '12vh' }}>
             {/* <div className="flex justify-center">
                 <img src={logo} alt="Logo" />
               </div> */}
