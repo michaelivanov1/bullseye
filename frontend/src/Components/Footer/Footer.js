f06c4cf54c03c649d249123dcafd5680a9ef6868
Michael Ivanov <michaelivanov153@gmail.com>
2024-02-03 19:57:18 -0500

    started footer component

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
 
     return (
-        <div className=" p-8 bg-black bg-opacity-20" style={{ height: '15vh' }}>
-
+        <div className="p-8 bg-black" style={{ height: '15vh' }}>
+            <h1 className="text-header">footer here</h1>
         </div>
     );
 }
