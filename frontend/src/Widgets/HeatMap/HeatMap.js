f542d0b54f5f2b8ed0f7bca09302b6f1897d8a2d
DMahyuddin <mahyuddin.22dylan@gmail.com>
2024-02-10 20:41:07 -0500

    fixed heatmap bug

--- frontend/src/Widgets/HeatMap/HeatMap.js
+++ frontend/src/Widgets/HeatMap/HeatMap.js
@@ -3,14 +3,16 @@ import React, { useEffect, useRef, memo } from "react";
 
 const HeatMapWidget = () => {
   const container = useRef();
+  const script = useRef();
 
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js";
-    script.type = "text/javascript";
-    script.async = true;
-    script.innerHTML = `
+    if (!script.current) {
+      script.current = document.createElement("script");
+      script.current.src =
+        "https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js";
+      script.current.type = "text/javascript";
+      script.current.async = true;
+      script.current.innerHTML = `
         {
           "exchanges": [],
           "dataSource": "SPX500",
@@ -27,22 +29,15 @@ const HeatMapWidget = () => {
           "width": "100%",
           "height": "100%"
         }`;
-    container.current.innerHTML = "";
-    container.current.appendChild(script);
+      container.current.innerHTML = "";
+      container.current.appendChild(script.current);
+    }
   }, []);
 
   return (
     <div className="tradingview-widget-container" ref={container}>
       <div className="tradingview-widget-container__widget"></div>
-      <div className="tradingview-widget-copyright">
-        <a
-          href="https://www.tradingview.com/"
-          rel="noopener nofollow"
-          target="_blank"
-        >
-          <span className="blue-text">Track all markets on TradingView</span>
-        </a>
-      </div>
+      <div className="tradingview-widget-copyright"></div>
     </div>
   );
 };
--- frontend/src/Widgets/TopStories/TopStories.js
+++ frontend/src/Widgets/TopStories/TopStories.js
@@ -35,15 +35,7 @@ const TopStoriesWidget = () => {
   return (
     <div className="tradingview-widget-container" ref={container}>
       <div className="tradingview-widget-container__widget"></div>
-      <div className="tradingview-widget-copyright">
-        <a
-          href="https://www.tradingview.com/"
-          rel="noopener nofollow"
-          target="_blank"
-        >
-          <span className="blue-text">Track all markets on TradingView</span>
-        </a>
-      </div>
+      <div className="tradingview-widget-copyright"></div>
     </div>
   );
 };
